395d9b7b22a87390482746c12d98cc344ff3235f
Westgeir <128185469+Westgeir@users.noreply.github.com>
2025-11-20 23:50:36 +0300

    config.php

--- config.php
+++ config.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $dbname = "записная книжка";
 
-// Создаем подключение
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Проверяем подключение
 if (!$conn) {
     die("Ошибка подключения: " . mysqli_connect_error());
 }
